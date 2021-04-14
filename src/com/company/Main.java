f49c887ec0d1552ab3464fe1f42fc47ea0122a08
antoine brivet <antoine.brivet@le-campus-numerique.fr>
2021-04-14 12:52:45 +0200

    Affichage des chiffres

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        // attributs
+        // la variable numbers avec pour valeur 123 va imprimer dans la console une suite de chiffre allant de 1 à 9.
+        int numbers = 123;
+
+
+        switch (numbers){
+
+            case 123:
+                System.out.println("      _   _           _    _   _    _    _");
+                System.out.println("  |   _|  _|   |_|   |_   |_    |  |_|  |_|");
+                System.out.println("  |  |_   _|     |    _|  |_|   |  |_|   _|");
+
+
+
+
+        }
+    }
+}
